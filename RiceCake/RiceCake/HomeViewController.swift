703b340a13baefc41c6bff09429ae7118e8b7060
JungYunseong <cysseo1@naver.com>
2022-07-14 21:55:03 +0900

    refactor: HomeViewController 설정
    기존 ViewController를 삭제하고 HomeViewController를 적용하였습니다.

--- RiceCake/RiceCake/HomeViewController.swift
+++ RiceCake/RiceCake/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  RiceCake
 //
 //  Created by Jung Yunseong on 2022/07/14.
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
 }
