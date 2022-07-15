63df3e2d2527f6b008164997e74845fd1ea894d0
JungYunseong <cysseo1@naver.com>
2022-07-15 13:05:50 +0900

    Merge branch 'feature/GameView' into develop

---
+++ RiceCake/RiceCake/Game/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  RiceCake
+//
+//  Created by Jung Yunseong on 2022/07/15.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
