5a14b2e1094636b93fce425c3694f12b33af0f6a
JungYunseong <cysseo1@naver.com>
2022-07-16 18:34:00 +0900

    refactor: didMove 함수 리팩토링
    - didMove에 추가 되어있던 Node들을 리팩토링 하였습니다.
    - 각 Node간의 zPosition을 수정하였습니다.

--- RiceCake/RiceCake/Game/GameScene.swift
+++ RiceCake/RiceCake/Game/GameScene.swift
@@ -14,13 +14,27 @@ class GameScene: SKScene {
     var player: SKSpriteNode = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-        
+        createPlayer()
+        createEnvironment()
+        createTouchAreaNode()
+    }
+    
+    func createPlayer() {
         player = SKSpriteNode(imageNamed: "player")
         player.size = CGSize(width: 20, height: 30)
         player.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        player.zPosition = 2
         self.addChild(player)
-        creatEnvironment()
-        
+    }
+    
+    func createEnvironment() {
+        let background = SKSpriteNode(color: .gray, size: CGSize(width: self.size.width, height: self.size.height))
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        background.zPosition = 1
+        self.addChild(background)
+    }
+    
+    func createTouchAreaNode() {
         self.touchAreaNode = SKShapeNode.init(circleOfRadius: 4)
         
         if let touchAreaNode = self.touchAreaNode {
@@ -29,14 +43,8 @@ class GameScene: SKScene {
             touchAreaNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
+            touchAreaNode.zPosition = 3
         }
-    }
-    
-    func creatEnvironment() {
-        let background = SKSpriteNode(color: .white, size: CGSize(width: self.size.width, height: self.size.height))
-        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        background.zPosition = 3
-        self.addChild(background)
     }
     
     func touchDown(atPoint pos: CGPoint) {
