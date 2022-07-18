038e9429cd81c9f64ffa633bbad95fbebf007d27
seodam-hst <seodam.hst@gmail.com>
2022-07-18 23:04:38 +0900

    feat: 아이 손을 추가하고 터치한 곳으로 이동하도록 수정

--- RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
+++ RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
@@ -9,6 +9,8 @@ import SpriteKit
 
 class BusSeatMissionScene: SKScene {
     
+    var childRightHand: SKSpriteNode = SKSpriteNode(imageNamed: "childRightHand")
+    
     //
     override func didMove(to view: SKView) {
 //        self.physicsWorld.contactDelegate = self
@@ -17,20 +19,48 @@ class BusSeatMissionScene: SKScene {
          drawBackground()
         
         // 아이의 손 (특정 위치에 가면 모양이 바뀌고 고정)
-        // createPlayerRightHand
+         drawChildRightHand()
+        
     }
     
     func drawBackground() {
         let busSeatMissionBackground = SKSpriteNode(imageNamed: "busSeatMissionBackground")
-        busSeatMissionBackground.size = CGSize(width: self.size.width,
-                                               height: self.size.height)
-        busSeatMissionBackground.position = CGPoint(x: self.size.width / 2,
-                                                    y: self.size.height / 2)
+        busSeatMissionBackground.size = CGSize(width: self.size.width, height: self.size.height)
+        busSeatMissionBackground.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
         busSeatMissionBackground.zPosition = BusSeatMissionLayer.busSeatMissionBackground
         
         self.addChild(busSeatMissionBackground)
     }
     
+    func drawChildRightHand() {
+        let handHeight = self.size
+            .width / 2
+        let handWidth = handHeight * (324 / 1000)
+        
+        childRightHand.size = CGSize(width: handWidth, height: handHeight)
+        childRightHand.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        childRightHand.zPosition = BusSeatMissionLayer.playerRightHand
+        
+        childRightHand.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: handWidth, height: handHeight))
+        childRightHand.physicsBody?.affectedByGravity = false
+//        childRightHand.physicsBody?.is
+        
+        self.addChild(childRightHand)
+    }
+    
+    func dragGesture(atPoint position: CGPoint) {
+//        let movementSpeed = 200.0
+//        let xPosition = position.x - childRightHand.position.x
+//        let yPosition = position.y - childRightHand.position.y
+        
+        childRightHand.run(SKAction.move(to: position, duration: 0.5))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            self.dragGesture(atPoint: touch.location(in: self))
+        }
+    }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
