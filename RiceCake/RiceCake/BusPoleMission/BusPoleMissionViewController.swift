35201e03f283dd86cda5b34748168c66c56cb3ad
bee712 <48711413+bee712@users.noreply.github.com>
2022-07-18 15:18:15 +0900

    feat[#16]: 파일 및 폴더 생성
    -BusPoleMission을 위한 스토리보드 및 VC 생성
    - 두 파일을 BusPoleMission 폴더에 넣음

---
+++ RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BusPoleMissionViewController.swift
+//  RiceCake
+//
+//  Created by Eunbi Han on 2022/07/18.
+//
+
+import UIKit
+
+class BusPoleMissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
