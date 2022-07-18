9004dfef09fea0d83ecacaf5944796393fca9657
seodam-hst <seodam.hst@gmail.com>
2022-07-18 21:29:44 +0900

    feat: BusSeatMission 배경화면 추가

--- RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
+++ RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
@@ -7,17 +7,31 @@
 
 import SpriteKit
 
-class BusSeatMissionScene: SKScene, SKPhysicsContactDelegate {
+class BusSeatMissionScene: SKScene {
     
     //
     override func didMove(to view: SKView) {
-        self.physicsWorld.contactDelegate = self
+//        self.physicsWorld.contactDelegate = self
         
         // 배경화면
+         drawBackground()
+        
         // 아이의 손 (특정 위치에 가면 모양이 바뀌고 고정)
+        // createPlayerRightHand
     }
     
-    private func drawBackground() {
+    func drawBackground() {
+        let busSeatMissionBackground = SKSpriteNode(imageNamed: "busSeatMissionBackground")
+        busSeatMissionBackground.size = CGSize(width: self.size.width,
+                                               height: self.size.height)
+        busSeatMissionBackground.position = CGPoint(x: self.size.width / 2,
+                                                    y: self.size.height / 2)
+        busSeatMissionBackground.zPosition = BusSeatMissionLayer.busSeatMissionBackground
         
+        self.addChild(busSeatMissionBackground)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
     }
 }
