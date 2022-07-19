6af930b9c06638adcb5e19b80c2bed74d0cb96fd
JungYunseong <cysseo1@naver.com>
2022-07-19 23:35:43 +0900

    fix: orthogonalScrollingBehavior 수정
     .groupPagingCentered이 잘 작동하지 않아 customLayout을 만들어 적용하였습니다.

--- RiceCake/RiceCake/StageView/StageViewController.swift
+++ RiceCake/RiceCake/StageView/StageViewController.swift
@@ -10,41 +10,25 @@ import UIKit
 class StageViewController: UIViewController {
     
     let lists: [Stage] = Stage.list
-    
-    var index = 0
-    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    
-    typealias Item = Stage
-    enum Section {
-        case main
-    }
+    var index: Int = 0
     
     @IBOutlet weak var stageNameLabel: UILabel!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
+    // 커스텀 레이아웃
+    let flowLayout = PaggedFlowLayout()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StageCell", for: indexPath) as? StageCell else {
-                return nil
-            }
-            
-            cell.configure(item)
-            return cell
-        })
-        
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(lists, toSection: .main)
-        dataSource.apply(snapshot)
-        
-        collectionView.collectionViewLayout = layout()
-        collectionView.alwaysBounceVertical = false
-        
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.decelerationRate = .fast
+        collectionView.contentInsetAdjustmentBehavior = .never
         collectionView.delegate = self
+        stageNameLabel.text = Stage.list[index].stageName
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,38 +37,12 @@ class StageViewController: UIViewController {
         navigationController?.isNavigationBarHidden = true
     }
     
-    private func layout() -> UICollectionViewCompositionalLayout {
-        
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 90
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        
-        section.visibleItemsInvalidationHandler = { [self] (_, offset, _) in
-            self.index = Int((offset.x + 654) / 465)
-            if self.index > lists.count - 1 {
-            } else {
-                self.stageNameLabel.text = Stage.list[self.index].stageName
-            }
-            
-            print(self.index)
-        }
-        
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
-    }
-    
     @IBAction func leftButtonTapped(_ sender: UIButton) {
-        if index == 0 {
-            index = 0
-        } else {
-            index -= 1
+        if index != 0 {
+            index += -1
             stageNameLabel.text = Stage.list[index].stageName
+        } else {
+            index = 0
         }
         
         let indexPath = IndexPath(item: index, section: 0)
@@ -92,11 +50,11 @@ class StageViewController: UIViewController {
     }
     
     @IBAction func rightButtonTapped(_ sender: UIButton) {
-        if index == lists.count - 1 {
-            index = lists.count - 1
-        } else {
+        if index != lists.count - 1 {
             index += 1
             stageNameLabel.text = Stage.list[index].stageName
+        } else {
+            index = lists.count - 1
         }
         
         let indexPath = IndexPath(item: index, section: 0)
@@ -104,6 +62,31 @@ class StageViewController: UIViewController {
     }
 }
 
+extension StageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return lists.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StageCell", for: indexPath) as? StageCell else {
+            return UICollectionViewCell()
+        }
+        
+        let stage = Stage.list[indexPath.item]
+        cell.configure(stage)
+        
+        return cell
+    }
+}
+
+extension StageViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page: Int = Int(scrollView.contentOffset.x / view.bounds.height)
+        stageNameLabel.text = Stage.list[page].stageName
+        self.index = page
+    }
+}
+    
 extension StageViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "GameView", bundle: nil)
