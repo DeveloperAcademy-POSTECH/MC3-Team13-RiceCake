86f6297485b3cda73d8fdcd0fee9250aa17e8c0b
bee712 <48711413+bee712@users.noreply.github.com>
2022-07-19 17:26:16 +0900

    feat: BusPoleMissonViewController 변수명 변경
    child -> busPoleMissionViewController 로 변경

--- RiceCake/RiceCake/Game/GameViewController.swift
+++ RiceCake/RiceCake/Game/GameViewController.swift
@@ -20,12 +20,11 @@ class GameViewController: UIViewController {
               
         // UIKit: BusPoleMissionViewController 연결
         let storyboard = UIStoryboard(name: "BusPoleMission", bundle: .main)
-        if let child = storyboard.instantiateViewController(identifier: "BusPole") as? BusPoleMissionViewController {
-            addChild(child)
-            missionView.addSubview(child.view)
-            child.didMove(toParent: self)
-            child.view.frame = missionView.bounds
-        }
+        let busPoleMissionViewController = storyboard.instantiateViewController(identifier: "BusPole") as! BusPoleMissionViewController
+        addChild(busPoleMissionViewController)
+        missionView.addSubview(busPoleMissionViewController.view)
+        busPoleMissionViewController.didMove(toParent: self)
+        busPoleMissionViewController.view.frame = missionView.bounds
         
         // SpriteKit: Scene을 MissionView에 연결
         //let missionScene: BusSeatMissionScene = BusSeatMissionScene(size: missionView.frame.size)
