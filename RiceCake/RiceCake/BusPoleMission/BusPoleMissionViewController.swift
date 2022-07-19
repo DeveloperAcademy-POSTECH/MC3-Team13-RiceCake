de6a0b6c2823f2992c020c598bbc7d1bec3d6701
bee712 <48711413+bee712@users.noreply.github.com>
2022-07-19 15:49:27 +0900

    feat: GameViewController에 BusPoleMissonViewController 연결
    MissionView에 BusPoleMission의 View를 보이고 BusPoleMissionViewController를 연결함

--- RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
+++ RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
@@ -27,5 +27,4 @@ class BusPoleMissionViewController: UIViewController {
         }
     }
     
-
 }
--- RiceCake/RiceCake/Game/GameViewController.swift
+++ RiceCake/RiceCake/Game/GameViewController.swift
@@ -19,10 +19,19 @@ class GameViewController: UIViewController {
         storyView.presentScene(scene)
         
         // https://stackoverflow.com/questions/24038215/how-to-navigate-from-one-view-controller-to-another-using-swift
-        let busSeatMissionVC = UIStoryboard.init(name: "BusSeatMission", bundle: Bundle.main).instantiateViewController(withIdentifier: "BusSeatMissionVC") as! BusSeatMissionViewController
-        busSeatMissionVC.view.frame = missionView.bounds
-        missionView.addSubview(busSeatMissionVC.view)
-        busSeatMissionVC.didMove(toParent: self)
+        //        let busSeatMissionVC = UIStoryboard.init(name: "BusSeatMission", bundle: Bundle.main).instantiateViewController(withIdentifier: "BusSeatMissionVC") as! BusSeatMissionViewController
+        //        busSeatMissionVC.view.frame = missionView.bounds
+        //        missionView.addSubview(busSeatMissionVC.view)
+        //        busSeatMissionVC.didMove(toParent: self)
+        
+        // MissionView에 BusPoleMissionViewController 연결
+        let storyboard = UIStoryboard(name: "BusPoleMission", bundle: .main)
+        if let child = storyboard.instantiateViewController(identifier: "BusPole") as? BusPoleMissionViewController {
+            addChild(child)
+            missionView.addSubview(child.view)
+            child.didMove(toParent: self)
+            child.view.frame = missionView.bounds
+        }
         
     }
     
