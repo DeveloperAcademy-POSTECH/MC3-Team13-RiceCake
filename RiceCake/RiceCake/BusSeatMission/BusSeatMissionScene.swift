c96ecff29539e1205e21cbbfae4a4c7a2bc93c0b
seodam-hst <seodam.hst@gmail.com>
2022-07-20 17:54:05 +0900

    refactor: Neis' Style Guide
    들여쓰기 교정;
    변하지 않는 변수를 let으로 정의;
    줄의 끝에 주석을 달아서 깔끔하게 보이도록 함.

--- RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
+++ RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
@@ -12,23 +12,18 @@ class BusSeatMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContac
     
     // 버스시트 배경, 아이의 손 노드 정의.
     let busSeatMissionBackground = SKSpriteNode(imageNamed: "busSeatMissionBackground")
-    var childRightHand: SKSpriteNode = SKSpriteNode(imageNamed: "childRightHand")
-    var grabbingHand: SKSpriteNode = SKSpriteNode(imageNamed: "grabbingHand")
+    let childRightHand: SKSpriteNode = SKSpriteNode(imageNamed: "childRightHand")
+    let grabbingHand: SKSpriteNode = SKSpriteNode(imageNamed: "grabbingHand")
     let seatHandle: SKSpriteNode = SKSpriteNode(imageNamed: "busSeatHandle")
     
     // Scene이 View에 그려질 때 수행할 작업들을 정의.
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
-        
-        // 배경화면 그리기
-        drawBackground()
-        
-        // 아이의 손을 그리고 드래그 제스쳐를 입력
-        drawHand()
-        drawGrabbingHand()
-        
-        // 가상의 프레임을 부여
-        drawSeatHandle()
+
+        drawBackground() // 배경화면 그리기
+        drawHand() // 움직이는 손 그리기
+        drawGrabbingHand() // 손잡이를 잡은 손 그리기
+        drawSeatHandle() // 좌석 손잡이 그리기
         
         // Long-press 제스쳐를 입력받는 변수를 선언하고 SKView에 추가.
         let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressHappened(sender:)))
@@ -45,8 +40,8 @@ class BusSeatMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContac
     // SKScene 위의 터치 위치가 변했을 때의 작업 정의.
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-                // 터치 위치를 따라 아이의 손이 움직이도록 변경
-                childRightHand.position = touch.location(in: self)
+            // 터치 위치를 따라 아이의 손이 움직이도록 변경
+            childRightHand.position = touch.location(in: self)
         }
     }
     override func update(_ currentTime: TimeInterval) {
