34c135d955c04a647e39861774c7829113624b3f
bee712 <48711413+bee712@users.noreply.github.com>
2022-07-21 02:25:36 +0900

    feat: BusPoleMisson에 LongPressGesture 추가 및 Gesture 코드 변경
    기존 코드로는 버그가 발생하여 기능을 함수로 구분하기 위해 현재 코드로 변경
    frontBusPole에 Tap, LongPress gesture 추가

--- RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
+++ RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
@@ -14,38 +14,48 @@ class BusPoleMissionViewController: UIViewController, UIGestureRecognizerDelegat
     @IBOutlet weak var secondBusHandle: UIImageView!
     @IBOutlet weak var frontBusPole: UIImageView!
     @IBOutlet weak var childLeftHand: UIImageView!
+    @IBOutlet weak var childHoldHand: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
-        firstBusHandleTapGesture.delegate = self
+        // 각 이미지에 gestureRecognizer 적용
+        let firstBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
+        firstBusHandle.addGestureRecognizer(firstBusHandleTapGesture)
         
-        let secondBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
-        secondBusHandleTapGesture.delegate = self
+        let secondBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
+        secondBusHandle.addGestureRecognizer(secondBusHandleTapGesture)
         
-//        let busPoleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
-//        busPoleTapGesture.delegate = self
+        let busPoleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
+        frontBusPole.tag = 1
+        frontBusPole.addGestureRecognizer(busPoleTapGesture)
         
-        self.firstBusHandle.addGestureRecognizer(firstBusHandleTapGesture)
-        self.secondBusHandle.addGestureRecognizer(secondBusHandleTapGesture)
-//        self.frontBusPole.addGestureRecognizer(busPoleTapGesture)
-        
+        let busPoleLongPressGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressedBusPole(_:)))
+        frontBusPole.addGestureRecognizer(busPoleLongPressGesture)
+ 
     }
     
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        handleTouched()
-        viewMoved(true)
-        self.firstBusHandle.endEditing(true)
-        self.secondBusHandle.endEditing(true)
-        
-        return true
+    @objc func tappedHandle(_ sender: UITapGestureRecognizer) {
+        childHandUp()
+        childHandDown()
+        sender.view?.tag ?? 0 != 1 ? viewMoved(true) : ()
     }
     
-    func handleTouched() {
+    @objc func longPressedBusPole(_ sender: UILongPressGestureRecognizer) {
+        childHandUp()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
+            self.childLeftHand.isHidden = true
+            self.childHoldHand.isHidden = false
+        }
+    }
+    
+    func childHandUp() {
         UIView.animate(withDuration: 0.7, animations: {
             self.childLeftHand.center.y -= self.view.bounds.height/4
         }, completion: nil)
+    }
+    
+    func childHandDown() {
         UIView.animate(withDuration: 0.7, delay: 0.7, animations: {
             self.childLeftHand.center.y += self.view.bounds.height/4
         }, completion: nil)
@@ -53,21 +63,25 @@ class BusPoleMissionViewController: UIViewController, UIGestureRecognizerDelegat
     
     func viewMoved(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
         UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
             self.busPoleBackground.center.x -= self.view.bounds.width
         }, completion: nil)
-
+        
         UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
             self.firstBusHandle.center.x -= self.view.bounds.width
         }, completion: nil)
-
+        
         UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
             self.secondBusHandle.center.x -= self.view.bounds.width
         }, completion: nil)
         
         UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
             self.frontBusPole.center.x -= self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
+            self.childHoldHand.center.x -= self.view.bounds.width
         }, completion: nil)
     }
 }
