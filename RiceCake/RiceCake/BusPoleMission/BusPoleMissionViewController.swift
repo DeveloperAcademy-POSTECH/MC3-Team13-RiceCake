663f19d8993e7dee2aa47e12b73ee81a3de4f86e
Eunbi Cho <moomb70@gmail.com>
2022-07-20 15:50:54 +0900

    feat: BusPoleMission에 탭 제스처 애니메이션
    테스트용 코드

--- RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
+++ RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
@@ -7,10 +7,64 @@
 
 import UIKit
 
-class BusPoleMissionViewController: UIViewController {
-    
+class BusPoleMissionViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet weak var busPoleBackground: UIImageView!
+    @IBOutlet weak var firstBusHandle: UIImageView!
+    @IBOutlet weak var frontBusPole: UIImageView!
+    @IBOutlet weak var childLeftHand: UIImageView!
+    @IBOutlet weak var secondBusHandle: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let busHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        busHandleTapGesture.delegate = self
+        
+        self.firstBusHandle.addGestureRecognizer(busHandleTapGesture)
+        self.secondBusHandle.addGestureRecognizer(busHandleTapGesture)
+        
     }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+//        handleTouched()
+//        viewMoved(true)
+        print("")
+        self.firstBusHandle.endEditing(true)
+        self.secondBusHandle.endEditing(true)
+        
+        return true
+    }
+    
+//    func handleTouched() {
+//        var frame = childLeftHand.frame
+//        frame.origin = CGPoint(x: 120, y: 456)
+//
+//        UIView.animate(withDuration: 1.0, animations: {
+//            self.childLeftHand.frame = frame
+//        }, completion: { finished in
+//            UIView.animate(withDuration: 1.0) {
+//                self.reset()
+//            }
+//        })
+//    }
+    
+//    func reset() {
+//        childLeftHand.frame = CGRect(x: 120, y: 700, width: 196, height: 588)
+//    }
+    
+//    func viewMoved(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//
+//        UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
+//            self.busPoleBackground.center.x -= self.view.bounds.width
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
+//            self.firstBusHandle.center.x -= self.view.bounds.width
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 1.5, options: [.curveEaseOut], animations: {
+//            self.secondBusHandle.center.x -= self.view.bounds.width
+//        }, completion: nil)
+//
+//    }
 }
