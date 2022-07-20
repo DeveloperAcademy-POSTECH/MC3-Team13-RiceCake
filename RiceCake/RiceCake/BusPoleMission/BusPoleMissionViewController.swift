52e59752aeb2e2ebd5b5b078e019bb6bb7773d6c
Eunbi Cho <moomb70@gmail.com>
2022-07-20 15:03:04 +0900

    feat: BusPoleMission 에셋 추가

--- RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
+++ RiceCake/RiceCake/BusPoleMission/BusPoleMissionViewController.swift
@@ -8,23 +8,9 @@
 import UIKit
 
 class BusPoleMissionViewController: UIViewController {
-
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet var tap: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tap = UITapGestureRecognizer(target: self, action: #selector(BusPoleMissionViewController.printHeart(_:)))
-        image.addGestureRecognizer(tap)
-        
     }
-    
-    // test function
-    @objc func printHeart(_ tap: UITapGestureRecognizer) {
-        if tap.state == .ended {
-            print("done")
-        }
-    }
-    
 }
