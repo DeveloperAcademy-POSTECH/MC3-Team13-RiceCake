3ca3782e11753b50bb715c6ab75cc724525a4ec6
seodam-hst <seodam.hst@gmail.com>
2022-07-20 16:25:23 +0900

    feat: 특정 영역에 손이 닿으면 손잡이를 잡는 모양으로 변경

--- RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
+++ RiceCake/RiceCake/BusSeatMission/BusSeatMissionScene.swift
@@ -15,15 +15,18 @@ class BusSeatMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContac
     // 버스시트 배경, 아이의 손 노드 정의.
     let busSeatMissionBackground = SKSpriteNode(imageNamed: "busSeatMissionBackground")
     var childRightHand: SKSpriteNode = SKSpriteNode(imageNamed: "childRightHand")
+    var grabbingHand: SKSpriteNode = SKSpriteNode(imageNamed: "grabbingHand")
     let wall: SKSpriteNode = SKSpriteNode(color: .green, size: CGSize(width: 20, height: 20))
     
     // Scene이 View에 그려질 때 수행할 작업들을 정의.
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        
         // 배경화면 그리기
         drawBackground()
         // 아이의 손을 그리고 드래그 제스쳐를 입력
-        drawChildRightHand()
+        drawHand()
+        drawGrabbingHand()
         // 가상의 프레임을 부여
         framewall()
         
@@ -67,12 +70,12 @@ class BusSeatMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContac
             childRightHand.isHidden = true
             //
             isTouchingHandle = true
+            grabbingHand.isHidden = false
         }
     }
 }
 // override 함수 외에 커스텀 함수들을 extension에 정의.
 extension BusSeatMissionScene {
-    //
     func drawBackground() {
         busSeatMissionBackground.size = CGSize(width: self.size.width + 4, height: self.size.height + 4)
         busSeatMissionBackground.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -87,8 +90,8 @@ extension BusSeatMissionScene {
         // add to Scene
         self.addChild(busSeatMissionBackground)
     }
-    //
     
+    // MARK: 손잡이로 대체하기
     func framewall() {
         wall.position = CGPoint(x: self.size.width / 2, y: self.size.height - 20)
         wall.zPosition = BusSeatMissionLayer.frameWall
@@ -100,12 +103,15 @@ extension BusSeatMissionScene {
         self.addChild(wall)
     }
     
-    func drawChildRightHand() {
-        let handHeight = self.size.width / 2
-        let handWidth = handHeight * (324 / 1000)
+    func drawHand() {
+        let handHeight = self.size.height
+        let handWidth = handHeight * (324 / 1600)
         
         childRightHand.size = CGSize(width: handWidth, height: handHeight)
-        childRightHand.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        childRightHand.position = CGPoint(
+            x: self.size.width / 2,
+            y: 0
+        )
         childRightHand.zPosition = BusSeatMissionLayer.playerRightHand
         childRightHand.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: handWidth, height: handHeight))
         childRightHand.physicsBody?.categoryBitMask = BusSeatPhysicsCategory.CategoryRightHand
@@ -114,6 +120,27 @@ extension BusSeatMissionScene {
         childRightHand.physicsBody?.isDynamic = true
         
         self.addChild(childRightHand)
+    }
+    
+    func drawGrabbingHand() {
+        grabbingHand.size = CGSize(
+            width: self.size.width,
+            height: self.size.height
+        )
+        grabbingHand.position = CGPoint(
+            x: self.size.width / 2,
+            y: self.size.height / 2
+        )
+        grabbingHand.zPosition = BusSeatMissionLayer.playerRightHand
+        grabbingHand.isHidden = true
+        
+        // 흔들림 효과 추가
+        let shakeLeft = SKAction.move(to: CGPoint(x: (self.size.width / 2) - 2, y: (self.size.height / 2) - 2), duration: 0.4)
+        let shakeRight = SKAction.move(to: CGPoint(x: (self.size.width / 2) + 2, y: (self.size.height / 2) + 2), duration: 0.4)
+        let shakeGrabbingHand = SKAction.sequence([shakeLeft, shakeRight])
+        grabbingHand.run(SKAction.repeatForever(shakeGrabbingHand))
+        
+        self.addChild(grabbingHand)
     }
     
     // Long-press 입력 시 수행할 작업 정의
