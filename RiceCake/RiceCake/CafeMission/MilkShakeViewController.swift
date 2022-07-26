ebb12bb2b36f2140d3e3f9e8b52a253762ba5a52
David-ShinDJ <dnrur124@gmail.com>
2022-07-26 19:18:56 +0900

    feat: Animation완성 -asset 완성도 높여서 추가해야함

--- RiceCake/RiceCake/CafeMission/MilkShakeViewController.swift
+++ RiceCake/RiceCake/CafeMission/MilkShakeViewController.swift
@@ -11,8 +11,11 @@ class MilkShakeMissionViewController: UIViewController {
     
     @IBOutlet weak var gestureTest: UIImageView!
     
+    @IBOutlet weak var animationView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animationView.alpha = 0
 
         let longPressedGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressedStarw(_:)))
         gestureTest.addGestureRecognizer(longPressedGesture)
@@ -24,15 +27,17 @@ class MilkShakeMissionViewController: UIViewController {
 //        firstBusHandle.addGestureRecognizer(firstBusHandleTapGesture)
     }
     //MARK: Animation 함수 넣기
-    
     @objc func tappedStraw(_ sender: UITapGestureRecognizer) {
         gestureTest.image = UIImage(named: "animation2")
         print("Tapped")
     }
-    
     @objc func longPressedStarw(_ sender: UILongPressGestureRecognizer) {
         print("LongPressed")
-        strawUpAndDown()
+        gestureTest.animationImages = animatedImages(for: "StrawAction")
+        gestureTest.animationDuration = 1.5
+        gestureTest.animationRepeatCount = 5
+        gestureTest.image = gestureTest.animationImages?.first
+        gestureTest.startAnimating()
     }
     
     @objc func panPressedStarw(_ sender: UIPanGestureRecognizer) {
@@ -40,23 +45,19 @@ class MilkShakeMissionViewController: UIViewController {
             self.gestureTest.image = UIImage(named: "animation4")
         }, completion: nil)
     }
-    func strawDownAction() {
-        self.gestureTest.image = UIImage(named: "animation3")
-        print("down")
-        self.gestureTest.isHidden = false
-    }
     
-    // MARK: Image Asset Chanage방식이 되지않아서 도형을 넣어서 Position값을 수정해야한다
-    func strawUpAndDown() {
-        UIView.animate(withDuration: 1, delay: 0, options: .repeat, animations: { [weak self] in
-            guard let self = self else { return }
-            self.gestureTest.image = UIImage(named: "animation4")
-            print("UpAndDown")
-        }, completion: {(_) in
-            self.gestureTest.image = UIImage(named: "animation3")
-            self.strawUpAndDown()
-            self.strawDownAction()
-        })
+    // MARK: Image Asset Chanage방식이 되지않아서 다른방식으로 애니메이션 제작
+    
+    func animatedImages(for name: String) -> [UIImage] {
+        
+        var count = 3
+        var images = [UIImage]()
+        
+        while let image = UIImage(named: "animation\(count)") {
+            images.append(image)
+            count += 1
+        }
+        return images
     }
     // 빨대를 들어가는 액체 CA레이어로 만들어서 Image를 그대로 가지든가 백터이미지를 그렷나 포지션 값을 조절가능
     // Opacity 조절해서 2장 이미지
