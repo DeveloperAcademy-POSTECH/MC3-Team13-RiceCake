d9a4e6d2719bc360fcfe0b4ab98097296c1d59e4
Eunbi Cho <moomb70@gmail.com>
2022-07-26 17:37:55 +0900

    feat: BusBellMission 랜덤한 높이로 손이 올라오는 기능 구현

--- RiceCake/RiceCake/BusBellMission/BusBellMissionViewController.swift
+++ RiceCake/RiceCake/BusBellMission/BusBellMissionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BusBellMissionViewController: UIViewController {
+class BusBellMissionViewController: UIViewController, UICollisionBehaviorDelegate {
 
     @IBOutlet weak var busBellMissionBackground: UIImageView!
     @IBOutlet weak var busBell: UIImageView!
@@ -15,16 +15,28 @@ class BusBellMissionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        childHandUpAndDown()
     }
     
-    func randomY () -> CGFloat {
-        let randCoordsY = CGFloat.random(in: 0...view.bounds.height/4)
-        return randCoordsY
+    // 손 위치 반복될 때마다 초기 설정
+    func setupChildHand() {
+        self.childLeftHand.center.y = self.view.bounds.height * 1.4
     }
     
+    // 손이 올라가는 위치 랜덤 값으로 형성
+    func randomY() -> CGFloat {
+        let randomCordsY = CGFloat.random(in: view.bounds.height/2...view.bounds.height/1.5)
+        return randomCordsY
+    }
+    
+    // 손이 랜덤한 높이로 올라갔다 내려오는 함수
     func childHandUpAndDown() {
-        UIView.animate(withDuration: 0.7, delay: 0, options: [.repeat, .autoreverse], animations: {
-            self.childLeftHand.center.y = self.randomY()
-        }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut, .autoreverse], animations: { [weak self] in
+            guard let self = self else { return }
+            self.childLeftHand.center.y -= self.randomY()
+        }, completion: { (done) in
+            self.setupChildHand()
+            self.childHandUpAndDown()
+        })
     }
 }
