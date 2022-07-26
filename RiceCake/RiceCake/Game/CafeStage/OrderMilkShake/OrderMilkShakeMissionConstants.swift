d260856208ba812638e7c61e544318234ffb3ec2
Seodam <102859746+seodam-hst@users.noreply.github.com>
2022-07-27 02:27:06 +0900

    refactor: 사용하지 않는 코드 제거

--- RiceCake/RiceCake/Game/CafeStage/OrderMilkShake/OrderMilkShakeMissionConstants.swift
+++ RiceCake/RiceCake/Game/CafeStage/OrderMilkShake/OrderMilkShakeMissionConstants.swift
@@ -12,9 +12,3 @@ struct OrderMilkShakeMissionLayer {
     static let menuBoard: CGFloat = 2
     static let magnifiedMenuBoard: CGFloat = 3
 }
-
-struct OrderMilkShakeMissionPhysicsCategory {
-    // 노트 충돌 비트마스크 지정.
-    static let camera: UInt32 = 0x1 << 1
-    static let ground: UInt32 = 0x1 << 2
-}
