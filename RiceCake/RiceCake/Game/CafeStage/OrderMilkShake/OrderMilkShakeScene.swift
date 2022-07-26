9b4e4c355ffa4825776487d2c559d04d9f6c11e1
seodam-hst <seodam.hst@gmail.com>
2022-07-26 23:11:44 +0900

    feat: 점프 모션 추가

--- RiceCake/RiceCake/Game/CafeStage/OrderMilkShake/OrderMilkShakeScene.swift
+++ RiceCake/RiceCake/Game/CafeStage/OrderMilkShake/OrderMilkShakeScene.swift
@@ -9,12 +9,13 @@ import SpriteKit
 import UIKit
 
 class OrderMilkShakeMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContactDelegate {
+    var jumpCount: Int = 0
     var isCheckingMenu: Bool = false
     
     // SKNode들 생성
     let orderMilkShakeMissionBackground = SKSpriteNode(imageNamed: "orderMilkShakeMissionBackground")
     let menuBoard: SKSpriteNode = SKSpriteNode(imageNamed: "menuBoard")
-//    let magnifiedMenuBoard: SKSpriteNode = SKSpriteNode(imageNamed: "grabbingHand")
+    //    let magnifiedMenuBoard: SKSpriteNode = SKSpriteNode(imageNamed: "grabbingHand")
     
     // Scene이 View에 그려질 때 수행할 작업들을 정의.
     override func didMove(to view: SKView) {
@@ -22,46 +23,33 @@ class OrderMilkShakeMissionScene: SKScene, UIGestureRecognizerDelegate, SKPhysic
         
         drawBackground() // 배경 그리기
         drawMenuBoard() // 메뉴판 그리기
-        //        drawMagnifiedMenu() // 손잡이를 잡은 손 그리기
+        //        drawMagnifiedMenuBoard() // 손잡이를 잡은 손 그리기
         
         addDoubleTapGestureRecognizer() // Double-Tap Gesture 추가
     }
-    // SKScene에 터치가 시작되었을 때의 작업 정의.
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for _ in touches {
-            
-        }
-    }
-    // SKScene 위의 터치 위치가 변했을 때의 작업 정의.
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for _ in touches {
-            
-        }
-    }
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-    func didBegin(_ contact: SKPhysicsContact) {
-        
-    }
-    // Double Tap Gesture를 입력받는 변수를 선언하고 SKView에 추가.
+    // DoubleTapGesture를 입력받는 변수를 선언하고 SKView에 추가.
     func addDoubleTapGestureRecognizer() {
-        //        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressHappened(sender:)))
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTapHappened(sender:)))
+        let recognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(doubleTapHappened(sender:))
+        )
+        recognizer.numberOfTapsRequired = 2
         recognizer.delegate = self
         view?.addGestureRecognizer(recognizer)
     }
-    
     // LongPressGesture 입력 시 수행할 작업 정의
     @objc func doubleTapHappened(sender: UITapGestureRecognizer) {
         
-        if sender.state == .began {
-            // 미션 성공 조건 달성
-            isCheckingMenu = true
-        }
-        if sender.state == .ended {
-            
-        }
+        // 미션 성공 조건 달성
+        isCheckingMenu = true
+        
+        let jumpHeight = viewHeight * (350 / 1600)
+        let jumpUpAction = SKAction.moveBy(x: 0, y: jumpHeight, duration: 0.4)
+        let jumpDownAction = SKAction.moveBy(x: 0, y: -jumpHeight, duration: 0.4)
+        let jumpSequence = SKAction.sequence([jumpDownAction, jumpUpAction])
+
+        orderMilkShakeMissionBackground.run(jumpSequence)
+        menuBoard.run(jumpSequence)
     }
 }
 
@@ -70,30 +58,33 @@ extension OrderMilkShakeMissionScene {
     // 앞좌석 배경
     private func drawBackground() {
         orderMilkShakeMissionBackground.size = CGSize(
-            width: self.size.width,
-            height: self.size.height
+            width: viewWidth,
+            height: viewHeight * ( 1950 / 1600 )
         )
         orderMilkShakeMissionBackground.position = CGPoint(
-            x: self.size.width / 2,
-            y: self.size.height / 2
+            x: viewWidth / 2,
+            y: viewHeight / 2 + viewHeight * ( 175 / 1600 )
         )
         orderMilkShakeMissionBackground.zPosition = OrderMilkShakeMissionLayer.orderMilkShakeMissionBackground
-        
         self.addChild(orderMilkShakeMissionBackground)
     }
     
-    // 좌석 손잡이를 잡은 플레이어의 손
+    // 탁상용 메뉴판 그리기
     private func drawMenuBoard() {
         menuBoard.size = CGSize(
-            width: self.size.width,
-            height: self.size.height
+            width: viewWidth,
+            height: viewHeight * ( 1950 / 1600 )
         )
         menuBoard.position = CGPoint(
-            x: self.size.width / 2,
-            y: self.size.height / 2
+            x: viewWidth / 2,
+            y: viewHeight / 2 + viewHeight * ( 175 / 1600 )
         )
         menuBoard.zPosition = OrderMilkShakeMissionLayer.menuBoard
-        
         self.addChild(menuBoard)
     }
+}
+
+extension SKScene {
+    var viewWidth: CGFloat { size.width }
+    var viewHeight: CGFloat { size.height }
 }
