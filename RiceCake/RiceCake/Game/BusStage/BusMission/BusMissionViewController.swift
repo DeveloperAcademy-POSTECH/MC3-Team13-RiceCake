60cdbbab740cf4ffb3ed0679a36d3680adf8d436
seodam-hst <seodam.hst@gmail.com>
2022-07-27 17:03:40 +0900

    refactor: 함수 내에서 직접 View/Scene을 그리도록 수정 (enum 제거)

--- RiceCake/RiceCake/Game/BusStage/BusMission/BusMissionViewController.swift
+++ RiceCake/RiceCake/Game/BusStage/BusMission/BusMissionViewController.swift
@@ -15,83 +15,54 @@ class BusMissionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: Notification Post를 받았을 때 View/Scene을 전환하기
         NotificationCenter.default.addObserver(self, selector: #selector(drawBusSeatMission), name: .drawBusSeatMission, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(drawBusPoleMission), name: .drawBusPoleMission, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(eraseBusMission), name: .eraseBusMission, object: nil)
-        
-        let missionHintScene: HintScene = HintScene(size: missionView.frame.size)
-        missionView.presentScene(missionHintScene)
     }
     
-    // MARK: Notification Post를 받았을 때 View/Scene을 전환하기
-    // 프로퍼티 옵저버인 willSet을 사용하여 프로퍼티가 변경될 때 화면을 갱신합니다.
-    var busStageProgress: BusStageProgress = .busEnterHint {
-        willSet {
-            switch newValue {
-            
-            case .busEnterHint:
-                presentBusHintScene()
-                
-            case .seatHint:
-                presentBusHintScene()
-                
-            case .seatMission:
-                // SpriteKit: missionView에 BusSeatMissionScene을 띄웁니다.
-                let seatMissionScene: BusSeatMissionScene = BusSeatMissionScene(size: missionView.frame.size)
-                missionView.presentScene(seatMissionScene)
-                
-            case .poleHint:
-                presentBusHintScene()
-                
-            case .poleMission:
-                // UIKit: missionView에 BusPoleMissionView를 연결합니다.
-                let storyboard = UIStoryboard(name: "BusPoleMission", bundle: .main)
-                if let child = storyboard.instantiateViewController(identifier: "BusPole") as? BusPoleMissionViewController {
-                    addChild(child)
-                    missionView.addSubview(child.view)
-                    child.didMove(toParent: self)
-                    child.view.frame = missionView.bounds
-                }
-            }
-        }
-    }
-    // SpriteKit: missionView에 HintScene을 띄웁니다.
-    private func presentBusHintScene() {
-        let missionHintScene: HintScene = HintScene(size: missionView.frame.size)
-        missionView.presentScene(missionHintScene)
-    }
-    
-    // MARK: 가능한 MissionView의 상태를 나타냅니다.
-    enum BusStageProgress {
-        case busEnterHint,
-             seatMission,
-             seatHint,
-             poleMission,
-             poleHint
-    }
-    
-    // MARK: 각 BusStageProgress 때 실행할 함수를
+    // MARK: 각 힌트, 미션에 해당하는 View/Scene을 그리는 함수들
     @objc func drawBusEnterHint() {
-        self.busStageProgress = .seatHint
+        eraseBusMission()
+        // SpriteKit: missionView에 HintScene을 띄웁니다.
+        presentBusHintScene()
     }
     @objc func drawBusSeatHint() {
-        self.busStageProgress = .seatHint
+        eraseBusMission()
+        presentBusHintScene()
     }
     @objc func drawBusSeatMission() {
-        self.busStageProgress = .seatMission
+        eraseBusMission()
+        // SpriteKit: missionView에 BusSeatMissionScene을 띄웁니다.
+        let seatMissionScene: BusSeatMissionScene = BusSeatMissionScene(size: missionView.frame.size)
+        missionView.presentScene(seatMissionScene)
     }
     @objc func drawBusPoleHint() {
-        self.busStageProgress = .poleHint
+        eraseBusMission()
+        presentBusHintScene()
     }
     @objc func drawBusPoleMission() {
-        self.busStageProgress = .poleMission
+        eraseBusMission()
+        // UIKit: missionView에 BusPoleMissionView를 연결합니다.
+        let storyboard = UIStoryboard(name: "BusPoleMission", bundle: .main)
+        if let child = storyboard.instantiateViewController(identifier: "BusPole") as? BusPoleMissionViewController {
+            addChild(child)
+            missionView.addSubview(child.view)
+            child.didMove(toParent: self)
+            child.view.frame = missionView.bounds
+        }
     }
-    @objc func eraseBusMission() {
+    
+    // MARK: View/Scene을 더 편하게 그릴 수 있게 도와주는 함수들
+    private func eraseBusMission() {
         // UIKit: missionView의 모든 subView를 지웁니다.
         for view in self.missionView.subviews {
             view.removeFromSuperview()
         }
         // SpriteKit: missionView에 HintScene을 띄웁니다.
+        let missionHintScene: HintScene = HintScene(size: missionView.frame.size)
+        missionView.presentScene(missionHintScene)
+    }
+    private func presentBusHintScene() {
         let missionHintScene: HintScene = HintScene(size: missionView.frame.size)
         missionView.presentScene(missionHintScene)
     }
