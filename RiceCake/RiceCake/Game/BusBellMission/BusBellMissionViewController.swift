1a481d1cf1e9477a1bd51df0b2a20ed3fc8446b3
Eunbi Cho <moomb70@gmail.com>
2022-07-28 10:21:49 +0900

    feat: BusBellMission 미션 구현

--- RiceCake/RiceCake/Game/BusBellMission/BusBellMissionViewController.swift
+++ RiceCake/RiceCake/Game/BusBellMission/BusBellMissionViewController.swift
@@ -7,50 +7,52 @@
 
 import UIKit
 
-class BusBellMissionViewController: UIViewController, UICollisionBehaviorDelegate {
-    private var dynamicAnimator: UIDynamicAnimator?
+class BusBellMissionViewController: UIViewController {
     
     @IBOutlet weak var busBellMissionBackground: UIImageView!
     @IBOutlet weak var busBell: UIImageView!
+    @IBOutlet weak var tapRecognizer: UIImageView!
     @IBOutlet weak var childLeftHand: UIImageView!
+    @IBOutlet weak var pushedButton: UIImageView!
+    
+    // 3가지 손 위치
+    let childLeftHandPisitions: [CGFloat] = [1, 1.1, 1.3]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        childHandUpAndDown()
-        let collisionBehavior = UICollisionBehavior(items: [busBell, childLeftHand])
-        collisionBehavior.collisionDelegate = self
-        collisionBehavior.setTranslatesReferenceBoundsIntoBoundary(with: .zero)
-        collisionBehavior.collisionMode = .everything
-        self.dynamicAnimator?.addBehavior(collisionBehavior)
+        
+        let busBellTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedBell(_:)))
+        tapRecognizer.addGestureRecognizer(busBellTapGesture)
+        
+        self.pushedButton.isHidden = true
+        self.tapRecognizer.isHidden = true
+        childHandUpDown()
     }
     
-    // 손 위치 반복될 때마다 초기 설정
-    func setupChildHand() {
-        self.childLeftHand.center.y = self.view.bounds.height * 1.4
+    @objc func tappedBell(_ sender: UITapGestureRecognizer) {
+        self.pushedButton.isHidden = false
+        print("what")
     }
     
-    // 손이 올라가는 위치 랜덤 값으로 형성
-    func randomY() -> CGFloat {
-        let randomCordsY = CGFloat.random(in: view.bounds.height/2...view.bounds.height/1.5)
-        return randomCordsY
-    }
-    
-    // 손이 랜덤한 높이로 올라갔다 내려오는 함수
-    func childHandUpAndDown() {
+    func childHandUpDown() {
         UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut, .autoreverse], animations: { [weak self] in
             guard let self = self else { return }
-            self.childLeftHand.center.y -= self.randomY()
-        }, completion: { (done) in
+            self.childLeftHand.center.y -= self.view.bounds.height * self.childLeftHandPisitions.randomElement()!
+            if self.childLeftHand.center.y ==  self.view.bounds.height / 0.5 - self.view.bounds.height * 1.3 {
+                self.tapRecognizer.isHidden = false
+                print("ok")
+            }
+        }, completion: {_ in
             self.setupChildHand()
-            self.childHandUpAndDown()
+            self.childHandUpDown()
         })
     }
     
-    func collisionBehavior(_ behavior: UICollisionBehavior,
-                           beganContactFor item: UIDynamicItem,
-                           withBoundaryIdentifier identifier: NSCopying?,
-                           at position: CGPoint) {
-      print("began contact boundary")
+    // 손 위치 초기 설정
+    func setupChildHand() {
+        self.childLeftHand.center.y = self.view.bounds.height / 0.5
+        self.pushedButton.isHidden = true
+        self.tapRecognizer.isHidden = true
     }
     
 }
