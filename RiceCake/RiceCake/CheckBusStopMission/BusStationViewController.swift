2e844fb733018bfc60f323ba50b36827221615b9
kimseunghoon <yoyo0421@naver.com>
2022-07-28 15:44:06 +0900

    Modified BusCheckMission code commit

--- RiceCake/RiceCake/CheckBusStopMission/BusStationViewController.swift
+++ RiceCake/RiceCake/CheckBusStopMission/BusStationViewController.swift
@@ -11,13 +11,13 @@ import UIKit
 
 class BusStationViewController: UIViewController, UIGestureRecognizerDelegate {
     
-//    @IBOutlet weak var buttonImage: UIImageView!
-//    @IBOutlet weak var buttonImage2: UIImageView!
-    
+
+    //노드 @IBOutlet 연결
     @IBOutlet weak var buttonImage: UIImageView!
     @IBOutlet weak var buttonImage2: UIImageView!
     @IBOutlet weak var background: UIImageView!
     
+    //Pinch gesture scale 값 설정
     var pinch = UIPinchGestureRecognizer()
     var recognizerscale:CGFloat = 1.0
     var maxscale:CGFloat = 2.0
@@ -26,22 +26,11 @@ class BusStationViewController: UIViewController, UIGestureRecognizerDelegate {
 override func viewDidLoad() {
     super.viewDidLoad()
     
+    //Pinch gesture 연결
     pinch = UIPinchGestureRecognizer(target: self, action: #selector(dopinch(_:)))
         self.view.addGestureRecognizer(pinch)
 
-    
-    
-   
-    
-    
-let config = UIImage.SymbolConfiguration(pointSize: 72)
-    _ = UIImage(systemName: "CheckBusMission.rightseat", withConfiguration: config)
-    
-    let config2 = UIImage.SymbolConfiguration(pointSize: 72)
-    _ = UIImage(systemName: "CheckBusMission.leftseat", withConfiguration: config2)
-    
-    
-    
+    //swipe gesture 연결
     let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(leftTapPressed(_:)))
             swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
             self.view.addGestureRecognizer(swipeLeft)
@@ -57,8 +46,7 @@ let config = UIImage.SymbolConfiguration(pointSize: 72)
     buttonImage2.addGestureRecognizer(rightTapGesture)
     }
    
-    // Animation
-    
+    //swipe gesture Animation 값 설정
     @objc func leftTapPressed(_ sender: UISwipeGestureRecognizer) {
         UIView.animate(withDuration: 0.3, animations: {
             self.buttonImage.center.x -= self.view.bounds.height/1.5
@@ -76,10 +64,9 @@ let config = UIImage.SymbolConfiguration(pointSize: 72)
     @objc func tappedHandle(_ sender: UISwipeGestureRecognizer) {
         Leftperson()
         Rightperson()
-//        sender.view?.tag ?? 0 != 1 ? viewMoved(true) : ()
     }
     
-    
+    //swipe 사람 애니메이션 Animation 값 설정
     func Leftperson() {
         UIView.animate(withDuration: 0.3, animations: {
             self.buttonImage.center.y -= self.view.bounds.height/1.5
@@ -91,15 +78,12 @@ let config = UIImage.SymbolConfiguration(pointSize: 72)
         }, completion: nil)
     }
     
-   
-
     @objc func respondToSwipeGestureMulti(_ gesture: UIGestureRecognizer) {
             if let swipeGesture = gesture as? UISwipeGestureRecognizer {
                 buttonImage.image
                 buttonImage2.image
                 
-                // 발생한 이벤트가 각 방향의 스와이프 이벤트라면 해당 이미지 뷰를 초록색 화살표 이미지로 변경
-                switch swipeGesture.direction {
+            switch swipeGesture.direction {
                     
                     case UISwipeGestureRecognizer.Direction.left:
                     buttonImage.image
@@ -110,65 +94,24 @@ let config = UIImage.SymbolConfiguration(pointSize: 72)
                 }
             }
         }
-
-@objc func dopinch(_ pinch: UIPinchGestureRecognizer) {
-    
-    guard background != nil else {return}
-    
-    if pinch.state == .began || pinch.state == .changed{
-        if(recognizerscale < maxscale && pinch.scale > 1.0){
-            background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
-            
-            recognizerscale *= pinch.scale
-            pinch.scale = 1.0
-    }
-        else if(recognizerscale > minscale && pinch.scale < 1.0){
-            background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
-            recognizerscale *= pinch.scale
-            pinch.scale = 1.0
+    //Pinchgesture Animation 값 설정
+    @objc func dopinch(_ pinch: UIPinchGestureRecognizer) {
+        
+        guard background != nil else {return}
+        
+        if pinch.state == .began || pinch.state == .changed{
+            if(recognizerscale < maxscale && pinch.scale > 1.0){
+                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
+                
+                recognizerscale *= pinch.scale
+                pinch.scale = 1.0
         }
-//        sender.view?.tag ?? 0 != 1 ? viewMoved(true) : ()
+            else if(recognizerscale > minscale && pinch.scale < 1.0){
+                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
+                recognizerscale *= pinch.scale
+                pinch.scale = 1.0
+            }
+        }
+    }
 }
-
-}
-}
-
-
-
-
-
-
-
-
-
-//    // 각 이미지에 gestureRecognizer 적용
-//    let firstBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
-//    firstBusHandle.addGestureRecognizer(firstBusHandleTapGesture)
-//
-//    let secondBusHandleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
-//    secondBusHandle.addGestureRecognizer(secondBusHandleTapGesture)
-//
-//    let busPoleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedHandle(_:)))
-//    frontBusPole.tag = 1
-//    frontBusPole.addGestureRecognizer(busPoleTapGesture)
-//
-//    let busPoleLongPressGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressedBusPole(_:)))
-//    frontBusPole.addGestureRecognizer(busPoleLongPressGesture)
-
-
-//// tap gesture에 반응할 함수
-//@objc func tappedHandle(_ sender: UITapGestureRecognizer) {
-//    childHandUp()
-//    childHandDown()
-//    sender.view?.tag ?? 0 != 1 ? viewMoved(true) : ()
-//}
-//
-//// long press gesture에 반응할 함수
-//@objc func longPressedBusPole(_ sender: UILongPressGestureRecognizer) {
-//    childHandUp()
-//    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
-//        self.childLeftHand.isHidden = true
-//        self.childHoldHand.isHidden = false
-//    }
-//}
 
