9a44f8c2252e61e0388784ffb1650be4cc12f772
seodam-hst <seodam.hst@gmail.com>
2022-07-28 23:20:17 +0900

    refactor: 변수명 정리, 사용하지 않는 코드 제거
    Co-Authored-By: seunghoonkim34 <86309591+seunghoonkim34@users.noreply.github.com>

--- RiceCake/RiceCake/Game/BusStage/BusMission/BusStationMission/BusStationViewController.swift
+++ RiceCake/RiceCake/Game/BusStage/BusMission/BusStationMission/BusStationViewController.swift
@@ -9,106 +9,68 @@ import Foundation
 import UIKit
 
 class BusStationViewController: UIViewController, UIGestureRecognizerDelegate {
-
-    //노드 @IBOutlet 연결
+    
+    // 노드 @IBOutlet 연결
     @IBOutlet weak var buttonImage: UIImageView!
     @IBOutlet weak var buttonImage2: UIImageView!
     @IBOutlet weak var background: UIImageView!
     
-    //Pinch gesture scale 값 설정
-    var pinch = UIPinchGestureRecognizer()
-    var recognizerscale:CGFloat = 1.0
-    var maxscale:CGFloat = 2.0
-    var minscale:CGFloat = 1.0
+    // Pinch gesture scale 값 설정
+    var pinchRecognizer = UIPinchGestureRecognizer()
+    var recognizerScale: CGFloat = 1.0
+    var maxScale: CGFloat = 2.0
+    var minScale: CGFloat = 1.0
     
-override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    //Pinch gesture 연결
-    pinch = UIPinchGestureRecognizer(target: self, action: #selector(dopinch(_:)))
-        self.view.addGestureRecognizer(pinch)
-
-    //swipe gesture 연결
-    let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(leftTapPressed(_:)))
-            swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-            self.view.addGestureRecognizer(swipeLeft)
-            
-            let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(rightTapPressed(_:)))
-            swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-            self.view.addGestureRecognizer(swipeRight)
-    
-    let leftTapGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(leftTapPressed(_:)))
-    buttonImage.addGestureRecognizer(leftTapGesture)
-    
-    let rightTapGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(rightTapPressed(_:)))
-    buttonImage2.addGestureRecognizer(rightTapGesture)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Pinch gesture 연결
+        pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinchHappened(_:)))
+        self.view.addGestureRecognizer(pinchRecognizer)
+        
+        // Swipe gesture 연결
+        let swipeToLeftRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeToLeftHappened(_:)))
+        swipeToLeftRecognizer.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeToLeftRecognizer)
+        
+        let swipeToRightRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swiftToRightHappend(_:)))
+        swipeToRightRecognizer.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeToRightRecognizer)
     }
-   
-    //swipe gesture Animation 값 설정
-    @objc func leftTapPressed(_ sender: UISwipeGestureRecognizer) {
+    
+    // Swipe Gesture Animation 값 설정
+    @objc func swipeToLeftHappened(_ sender: UISwipeGestureRecognizer) {
+        movePeopleToLeft()
+    }
+    @objc func swiftToRightHappend(_ sender: UISwipeGestureRecognizer) {
+        movePeopleToRight()
+    }
+    // Pinch Gesture Animation 값 설정
+    @objc func pinchHappened(_ pinch: UIPinchGestureRecognizer) {
+        guard background != nil else { return }
+        
+        if pinch.state == .began || pinch.state == .changed {
+            if recognizerScale < maxScale && pinch.scale > 1.0 {
+                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
+                recognizerScale *= pinch.scale
+                pinch.scale = 1.0
+            } else if recognizerScale > minScale && pinch.scale < 1.0 {
+                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
+                recognizerScale *= pinch.scale
+                pinch.scale = 1.0
+            }
+        }
+    }
+    
+    // 사람 Animation 값 설정
+    func movePeopleToLeft() {
         UIView.animate(withDuration: 0.3, animations: {
             self.buttonImage.center.x -= self.view.bounds.height/1.5
         }, completion: nil)
-        print("야 왼쪽사람 저리좀 비켜봐!.")
     }
-
-    @objc func rightTapPressed(_ sender: UISwipeGestureRecognizer) {
+    func movePeopleToRight() {
         UIView.animate(withDuration: 0.3, animations: {
             self.buttonImage2.center.x += self.view.bounds.height/1.5
         }, completion: nil)
-        print("야 오른쪽사람 저리좀 비켜봐!.")
-    }
-    
-    @objc func tappedHandle(_ sender: UISwipeGestureRecognizer) {
-        leftPerson()
-        rightPerson()
-    }
-    
-    //swipe 사람 애니메이션 Animation 값 설정
-    func leftPerson() {
-        UIView.animate(withDuration: 0.3, animations: {
-            self.buttonImage.center.y -= self.view.bounds.height/1.5
-        }, completion: nil)
-    }
-    func rightPerson() {
-        UIView.animate(withDuration: 0.3, animations: {
-            self.buttonImage2.center.y += self.view.bounds.height/1.5
-        }, completion: nil)
-    }
-    
-    @objc func respondToSwipeGestureMulti(_ gesture: UIGestureRecognizer) {
-            if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-                buttonImage.image
-                buttonImage2.image
-                
-            switch swipeGesture.direction {
-                    
-                    case UISwipeGestureRecognizer.Direction.left:
-                    buttonImage.image
-                    case UISwipeGestureRecognizer.Direction.right:
-                    buttonImage2.image
-                    default:
-                        break
-                }
-            }
-        }
-    //Pinchgesture Animation 값 설정
-    @objc func dopinch(_ pinch: UIPinchGestureRecognizer) {
-        
-        guard background != nil else {return}
-        
-        if pinch.state == .began || pinch.state == .changed{
-            if(recognizerscale < maxscale && pinch.scale > 1.0){
-                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
-                
-                recognizerscale *= pinch.scale
-                pinch.scale = 1.0
-        }
-            else if(recognizerscale > minscale && pinch.scale < 1.0){
-                background.transform = (background.transform).scaledBy(x: pinch.scale, y: pinch.scale)
-                recognizerscale *= pinch.scale
-                pinch.scale = 1.0
-            }
-        }
     }
 }
