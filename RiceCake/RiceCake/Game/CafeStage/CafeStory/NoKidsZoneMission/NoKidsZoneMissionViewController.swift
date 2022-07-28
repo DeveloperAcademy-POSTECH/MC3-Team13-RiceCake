466c7580faa2bc5f839353e777325d5bc786cb2c
bee712 <48711413+bee712@users.noreply.github.com>
2022-07-28 16:24:02 +0900

    feat: NoKidsZone 폴더 및 Storyboard, ViewController 생성

---
+++ RiceCake/RiceCake/Game/CafeStage/CafeStory/NoKidsZoneMission/NoKidsZoneMissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoKidsZoneMissionViewController.swift
+//  RiceCake
+//
+//  Created by Eunbi Han on 2022/07/28.
+//
+
+import UIKit
+
+class NoKidsZoneMissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
