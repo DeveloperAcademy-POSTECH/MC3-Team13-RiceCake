210f3440e8a7cefd22079e64781617e31bb320c8
JungYunseong <cysseo1@naver.com>
2022-07-30 18:59:40 +0900

    refactor: NotificationCenter
    사용하지 않는 Notification을 삭제하였습니다.

--- RiceCake/RiceCake/Game/StageNotificationCenter.swift
+++ RiceCake/RiceCake/Game/StageNotificationCenter.swift
@@ -9,41 +9,38 @@ import Foundation
 
 // NotificationCenter에 추가
 extension Notification.Name {
-    // BusStage
-    static let seatMission = Notification.Name("seatMission")
-    static let poleMission = Notification.Name("poleMission")
+    // MARK: - BusStage
+    // Bus Stage Mission
     static let searchForNextMission = Notification.Name("cancelMission")
-    
+    // Bus Stage 시작
     static let drawBusEnterHint = Notification.Name("drawBusEnterHint")
-    
+    // Bus Seat Mission
     static let drawBusSeatHint = Notification.Name("drawBusSeatHint")
     static let drawBusSeatMission = Notification.Name("drawBusSeatMission")
-    
+    // Bus Station Mission
     static let drawBusStationHint = Notification.Name("drawBusStationHint")
     static let drawBusStationMission = Notification.Name("drawBusStationMission")
-    
+    // Bus Bell Mission
     static let drawBusBellHint = Notification.Name("drawBusBellHint")
     static let drawBusBellMission = Notification.Name("drawBusBellMission")
-    
+    // Bus Pole Mission
     static let drawBusPoleHint = Notification.Name("drawBusPoleHint")
     static let drawBusPoleMission = Notification.Name("drawBusPoleMission")
-    
-    static let drawDrinkHint = Notification.Name("drawDrinkMilkShakeHint")
-    static let drawDrinkMission = Notification.Name("drawDrinkMilkShakeMission")
-    
+    // Erase Mission
     static let eraseBusMission = Notification.Name("eraseBusMission")
     
-    // CafeStage
+    // MARK: - CafeStage
+    // Cafe Stage Mission
+    static let drawSearchForAnotherCafeHint = Notification.Name("drawSearchForAnotherCafeHint")
+    // Cafe NokidsZone Mission
     static let drawCafeNoKidsZoneHint = Notification.Name("drawCafeNoKidsZoneHint")
     static let drawCafeNoKidsZoneMission = Notification.Name("drawCafeNoKidsZoneMission")
-    
-    static let drawSearchForAnotherCafeHint = Notification.Name("drawSearchForAnotherCafeHint")
-    
+    // Cafe Order Mission
     static let drawCafeOrderMilkShakeHint = Notification.Name("drawCafeOrderMilkShakeHint")
     static let drawCafeOrderMilkShakeMission = Notification.Name("drawCafeOrderMilkShakeMission")
-    
+    // Cafe Drink Mission
     static let drawCafeDrinkMilkShakeHint = Notification.Name("drawCafeDrinkMilkShakeHint")
     static let drawCafeDrinkMilkShakeMission = Notification.Name("drawCafeDrinkMilkShakeMission")
-    
+    // Erase Mission
     static let eraseCafeMission = Notification.Name("eraseCafeMission")
 }
